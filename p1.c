c8387aedc5164dafdf6abd95568c6f0594e7b79a
Negipallavi <149373681+Negipallavi@users.noreply.github.com>
2023-12-18 21:15:00 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b;
+    printf("[1] Teacher \n[2] student\n ");
+    scanf("%d",&a);
+
+    
+  if(a==1)printf(" my name is riya \n I teach all subjects from class 1 to 9. \n fee can be negociate \n contact no :9657xxxxxx \n locatio:any where(home tution)\n \n my name is rishi \n I teach science and maths from class 1 to 12. \n fee can be negociate \n contact no :9387xxxxxx \nlocation:cargii chawk");
+  else if(a==2)printf("my name is pallavi \n I want a home tution of science and maths \nlocation:-cargi chawk \ncontact no:-78965xxxxx\n \nmy name is amit \n I want a tution of science \nlocation:-near Rajiv Nagar \ncontact no:-84165xxxxx" );
+    
+printf("thankyou for visit");
+return 0;
+}
